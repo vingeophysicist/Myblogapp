4fe24f5b7a3e6c22b398a42b4e0abc4a8cbb5d7c
vingeophysicist <uobodomavin@gmail.com>
2021-04-24 00:20:13 +0100

    new changes

--- blogapp/blogapp/settings.py
+++ blogapp/blogapp/settings.py
@@ -94,8 +94,12 @@ WSGI_APPLICATION = 'blogapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'myblogapp',
+        'USER': 'postgres',
+        'PASSWORD': 'jambwaec',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
